3038f32578a06762ce4a60f8651e844f2b87d1f7
yangyq <yangyq@umich.edu>
2022-07-02 02:08:27 -0400

    add /

--- main.go
+++ main.go
@@ -47,8 +47,8 @@ func main() {
 	{
 		wallet_group.POST("", wallet.CreateWalletHandler)
 		// wallet_group.GET("", wallet.GetWalletsHandler)
-		wallet_group.DELETE(":id", wallet.DeleteWalletsHandler)
-		wallet_group.GET("*id", wallet.GetBalancesHandler)
+		wallet_group.DELETE("/:id", wallet.DeleteWalletsHandler)
+		wallet_group.GET("/*id", wallet.GetBalancesHandler)
 	}
 
 	log.Fatal(router.Run(":8080"))
