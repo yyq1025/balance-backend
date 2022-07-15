bc3c06f9427dafd22e14c80ff7f91f00f16b2d33
yangyq <yangyq@umich.edu>
2022-07-15 15:57:56 -0400

    support avax-c chain now

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO "networks" ("chain_id", "name", "url", "symbol", "explorer") VALUES
 ('0x1', 'Ethereum', 'https://eth.public-rpc.com', 'ETH', 'https://etherscan.io'),
 ('0x38', 'BSC', 'https://bsc-dataseed.binance.org/', 'BNB', 'https://bscscan.com'),
-('0xA86A', 'Avalanche', 'https://api.avax.network/ext/bc/C/rpc', 'AVAX', 'https://snowtrace.io'),
+('0xA86A', 'Avalanche-C', 'https://api.avax.network/ext/bc/C/rpc', 'AVAX', 'https://snowtrace.io'),
 ('0x89', 'Polygon', 'https://polygon-rpc.com', 'MATIC', 'https://polygonscan.com'),
 ('0x19', 'Cronos', 'https://evm.cronos.org', 'CRO', 'https://cronoscan.com'),
 ('0xFA', 'Fantom', 'https://rpc.ftm.tools/', 'FTM', 'https://ftmscan.com'),
