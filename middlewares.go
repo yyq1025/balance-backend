a25650da3726be5b99adcfdb7b6b55fc5d710d7c
yangyq <yangyq@umich.edu>
2022-07-15 14:16:37 -0400

    fix cors

--- middlewares.go
+++ middlewares.go
@@ -18,7 +18,7 @@ func corsMiddleware() gin.HandlerFunc {
 
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version")
+		c.Header("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version")
 		c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT")
 
 		if c.Request.Method == "OPTIONS" {
